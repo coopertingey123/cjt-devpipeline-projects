0e7632b402bac9beb3515ddac9f48d242ba60cc7
Cooper Tingey <coopertingey@Coopers-MBP.hsd1.ut.comcast.net>
2021-12-06 10:57:34 -0700

    added ability to give a user a competency

--- Capstone/user.py
+++ Capstone/user.py
@@ -8,6 +8,7 @@ cursor = connection.cursor()
 class User:
 
     def __init__(self):
+        self.date = datetime.date.today()
         option = input("Would you like to login or create an account? Enter (L)ogin/(C)reate/(Q)uit: ").lower()
         if option == "c":
             return User.add_user(self)
@@ -37,7 +38,9 @@ class User:
             )
         """
         values = self.first_name, self.last_name, self.email, hashed_password, self.phone, self.date_created, self.hire_date, self.user_type
-        return cursor.execute(sql_statement, values)
+        cursor.execute(sql_statement, values)
+        connection.commit()
+        return
     
     def hash_password(self, password):
         salt = bcrypt.gensalt()
@@ -54,13 +57,25 @@ class User:
         """
         emails = cursor.execute(sql_statement, (email,)).fetchone()
         if emails == None:
-            return print("No email found")
+            print("No email found")
+            try_again = input("Would you like to try again? Enter(y/n):").lower()
+            if try_again == 'y':
+                return User.login(self)
+            else:
+                return quit()
         elif bcrypt.checkpw(password.encode('utf-8'), emails[2]):
-            self.user_id = emails[1]
+            self.user_id = emails[0]
             self.user_type = emails[3]
+            if self.user_type == "manager":
+                Manager.show_actions(self)
             return print("Logged in successfully")
         else:
-            return print("Email or password incorrect.")
+            print("Email or password incorrect.")
+            try_again = input("Would you like to try again? Enter(y/n):").lower()
+            if try_again == 'y':
+                return User.login(self)
+            else: 
+                return quit()
 
     def show_actions(self):
         print("""
@@ -68,23 +83,43 @@ class User:
         2. View my completed assessments
         3. Edit my information
         """)
+        print(self.user_id)
         select = input("Select the number of the action you would like to take. (q to quit) Enter: ")
-        if select == 1:
-            return User.view_competencies()
-        elif select == 2:
-            return User.view_completed_assessments()
-        elif select == 3:
-            return User.edit_user_info()
+        if select == "1":
+            return User.view_competencies(self)
+        elif select == "2":
+            return User.view_completed_assessments(self)
+        elif select == "3":
+            return User.edit_user_info(self, self.user_id)
         elif select == 'q':
             return exit()
         else:
             select = input("Select the number of the action you would like to take. Enter: ")
     
     def view_competencies(self):
+        sql_statement = """
+            SELECT competency_id, name
+            FROM Competency
+        """
+        competencies = cursor.execute(sql_statement).fetchall()
+        print("Competency ID    Name/Description")
+        for competency in competencies:
+            print(f"{competency[0]:<16} {competency[1]}")
         return
 
-    def view_completed_assessments(self):
+    def view_assessments(self, competency_id):
+        sql_statement = """
+            SELECT assessment_id, name
+            FROM Assessment
+            WHERE competency_id = ?
+        """
+        assessments = cursor.execute(sql_statement, (competency_id,)).fetchall()
+        print("Assessment ID    Name/Description")
+        for assessment in assessments:
+            print(f"{assessment[0]:<16} {assessment[1]}")
         return
+
+    
 
     def edit_user_info(self, user_id):
         sql_statement = """
@@ -98,7 +133,58 @@ class User:
         print(f"3. Email: {user_info[2]}")
         print(f"4. Password: *****\n")
         select = input("Select the number of the field you would like to change. Enter: ")
-        
+        if select == "1":
+            new_value = input("Enter your new first name. Enter: ")
+            edit_statement = """
+                UPDATE User
+                SET first_name = ?
+                WHERE user_id = ?
+            """
+            values = new_value, user_id
+            cursor.execute(edit_statement, values)
+            print("\nFirst name successfully changed")
+        elif select == "2":
+            new_value = input("Enter your new last name. Enter: ")
+            edit_statement = """
+                UPDATE User
+                SET last_name = ?
+                WHERE user_id = ?
+            """
+            values = new_value, user_id
+            cursor.execute(edit_statement, values)
+            print("\nLast name successfully changed")
+        elif select == "3":
+            new_value = input("Enter your new email. Enter: ")
+            edit_statement = """
+                UPDATE User
+                SET email = ?
+                WHERE user_id = ?
+            """
+            values = new_value, user_id
+            cursor.execute(edit_statement, values)
+            print("\nField successfully changed")
+        elif select == "4":
+            old_pw = input("Enter your old password. Enter: ")
+            sql_statement = """
+                SELECT password
+                FROM User
+                WHERE user_id = ?
+            """
+            old_hashed_pw = cursor.execute(sql_statement, (user_id,)).fetchone()
+            if bcrypt.checkpw(old_pw.encode('utf-8'), old_hashed_pw[0]):
+                new_value = input("Enter your new password. Enter: ")
+                new_hashed_pw = User.hash_password(self, new_value)
+                edit_statement = """
+                    UPDATE User
+                    SET password = ?
+                    WHERE user_id = ?
+                """
+                values = new_hashed_pw, user_id
+                cursor.execute(edit_statement, values)
+                print("\nField successfully changed")
+            else:
+                print("\nIncorrect password")
+        connection.commit()  
         return
 
 
@@ -109,8 +195,63 @@ class User:
 
 class Manager(User):
 
-    def __init__(self):
-        self.date = datetime.date.today()
+    # def __init__(self):
+    #     self.date = datetime.date.today()
+    #     return
+
+    def show_actions(self):
+        print("""
+        As a manager, you can see, create, edit, and delete users, 
+        competencies, assessments, and reports. Select the number of the
+        section you want to access.""")
+        print("""
+        1. Users
+        2. Competencies
+        3. Reports
+        """)
+        select = input("Select the number of the section you would like to access. (q to quit) Enter: ")
+        if select == "1":
+            return User.view_competencies(self)
+        elif select == "2":
+            return User.view_completed_assessments(self)
+        elif select == "3":
+            return User.edit_user_info(self, self.user_id)
+        elif select == 'q':
+            return exit()
+        else:
+            select = input("Select the number of the action you would like to take. Enter: ")
+
+    def view_user_actions(self):
+        print("""
+        1. Create User/Manager
+        2. View all users
+        3. Edit a user
+        4. Search user by first/last name
+        5. View specific users and their assessments
+        6. View specific users and their competencies
+        """)
+        return
+
+    def view_competency_actions(self):
+        print("""
+        1. Create a competency
+        2. View competencies
+        3. Edit competencies
+        4. Create an assessment
+        5. View assessments
+        6. Edit assessments
+        7. View specific users and their competencies
+        8. View specific users and their assessments
+        """)
+        return
+
+    def view_reports_actions(self):
+        print("""
+        Reports contain a summary of users and their competency levels for a given competency
+
+        1. View All Users report 
+        2. View Individual User report
+        """)
         return
 
     def add_manager(self):
@@ -134,7 +275,48 @@ class Manager(User):
         """
         values = self.first_name, self.last_name, self.email, hashed_password, self.phone, date_created, hire_date, user_type
         return cursor.execute(sql_statement, values)
-    
+
+    def view_users(self):
+        sql_statement = """
+            SELECT user_id, first_name, last_name, phone, email
+            FROM User
+        """
+        users = cursor.execute(sql_statement).fetchall()
+        print("User ID   First Name       Last Name        Phone        Email")
+        for user in users:
+            print(f"{user[0]:<9} {user[1]:<16} {user[2]:<16} {user[3]:<12} {user[4]}")
+        return
+
+    def search_user(self):
+        search = input("Search User by first or last name. Search: ")
+        sql_statement = """
+            SELECT user_id, first_name, last_name, phone, email FROM User
+            WHERE first_name LIKE "%?%"
+            OR last_name LIKE "%?%"
+        """
+        values = search, search
+        filtered_users = cursor.execute(sql_statement, values).fetchall()
+        print("User ID   First Name       Last Name        Phone        Email")
+        for user in filtered_users:
+            print(f"{user[0]:<9} {user[1]:<16} {user[2]:<16} {user[3]:<12} {user[4]}")
+        user_id = input("Select a user ID to take action on that user's account.")
+        return self.select_action_after_user_select(self, user_id)
+
+    def select_action_after_user_select(self, user_id):
+        print("""
+            1. Edit User
+            2. View User Competencies
+            3. View User Assessments
+        """)
+        action = input("Select action number. Enter: ")
+        if action == "1":
+            return User.edit_user_info(self, user_id)
+        elif action == "2":
+            return 
+        elif action == "3":
+            return
+        return
+
     def create_compentency(self):
         name = input("Enter the name of the competency you'd like to create. Enter: ")
         date_created = self.date
@@ -163,7 +345,29 @@ class Manager(User):
         values = competency_id, name, date_created
         return cursor.execute(sql_statement, values)
 
-cooper = User()
-cooper.edit_user_info(1)
+    def give_user_a_competency(self, user_id):
+        User.view_competencies(self)
+        select_competency = input("Select a competency ID to view its assessments. Enter: ")
+        User.view_assessments(self, select_competency)
+        select_assessment = input("Select an assessment ID to give the user a score for the assessment. Enter: ")
+        assessment_score = input("What did the user score on this assessment? Enter: ")
+        sql_statement = """
+            INSERT INTO Assessment_Results (
+                assessment_id, user_id, score, date_taken, manager_id
+            )
+            VALUES (
+                ?, ?, ?, ?, ?
+            )
+        """
+        values = select_assessment, user_id, assessment_score, self.date, self.user_id
+        return cursor.execute(sql_statement, values)
+        #Here is where the data would go to the database.
+
+        return
+
+cooper = Manager()
+# cooper.edit_user_info()
+# cooper.view_competencies()
+cooper.give_user_a_competency(1)
 
 connection.commit()  
